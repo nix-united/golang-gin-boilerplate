ba97e6a738bed3d1d2ee6b56e2585d639e15b16f
oleksandrkhmil <oleksandr.khmil@sumup.com>
2024-12-20 16:46:09 +0200

    feat: add internal interface for user repository

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,10 +2,15 @@ package service
 
 import (
 	"basic_server/internal/model"
-	"basic_server/internal/repository"
 	"basic_server/internal/request"
 	"basic_server/internal/utils"
+	"fmt"
 )
+
+type userRepository interface {
+	FindUserByEmail(email string) (model.User, error)
+	StoreUser(user model.User) error
+}
 
 // UserServiceI provides a use case level for the user entity
 type UserServiceI interface {
@@ -16,21 +21,20 @@ type UserServiceI interface {
 }
 
 type UserService struct {
-	UserRepo repository.UserRepositoryI
+	userRepository userRepository
 }
 
 // NewUserService returns an instance of the UserService
-func NewUserService(ur repository.UserRepositoryI) UserServiceI {
-	return &UserService{
-		UserRepo: ur,
+func NewUserService(userRepository userRepository) UserService {
+	return UserService{
+		userRepository: userRepository,
 	}
 }
 
 func (srv UserService) CreateUser(req request.RegisterRequest, en utils.Encryptor) error {
-	user, err := srv.UserRepo.FindUserByEmail(req.Email)
-
+	user, err := srv.userRepository.FindUserByEmail(req.Email)
 	if err != nil {
-		return err
+		return fmt.Errorf("find user by email: %w", err)
 	}
 
 	if user.ID != 0 {
@@ -43,19 +47,17 @@ func (srv UserService) CreateUser(req request.RegisterRequest, en utils.Encrypto
 	var encryptedPassword string
 
 	encryptedPassword, err = en.Encrypt(req.Password)
-
 	if err != nil {
-		return err
+		return fmt.Errorf("encrypt password: %w", err)
 	}
 
-	err = srv.UserRepo.StoreUser(model.User{
+	err = srv.userRepository.StoreUser(model.User{
 		Email:    req.Email,
 		Password: encryptedPassword,
 		FullName: req.FullName,
 	})
-
 	if err != nil {
-		return err
+		return fmt.Errorf("store user: %w", err)
 	}
 
 	return nil
