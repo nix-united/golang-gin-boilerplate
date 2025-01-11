3cb215e0ff4678e72dc57dfba60d8644ffcffdf7
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-01-11 15:37:09 +0200

    test: test for save post

--- internal/handler/post_handler_test.go
+++ internal/handler/post_handler_test.go
@@ -1,6 +1,8 @@
 package handler_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -8,8 +10,10 @@ import (
 
 	"github.com/nix-united/golang-gin-boilerplate/internal/handler"
 	"github.com/nix-united/golang-gin-boilerplate/internal/model"
+	"github.com/nix-united/golang-gin-boilerplate/internal/request"
 	"github.com/nix-united/golang-gin-boilerplate/internal/service"
 
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -26,6 +30,10 @@ func newPostHandler(t *testing.T) (*gin.Engine, *MockpostService) {
 
 	engine := gin.New()
 	gin.SetMode(gin.TestMode)
+
+	engine.Use(func(c *gin.Context) {
+		c.Set("JWT_PAYLOAD", jwt.MapClaims{"id": float64(101)})
+	})
 
 	engine.POST("/posts", postHandler.SavePost)
 	engine.GET("/posts", postHandler.GetPosts)
@@ -56,6 +64,54 @@ func TestPostHandler_GetPostByID(t *testing.T) {
 		})
 
 	httpRequest := httptest.NewRequest(http.MethodGet, "/post/100", http.NoBody)
+
+	recorder := httptest.NewRecorder()
+	engine.ServeHTTP(recorder, httpRequest)
+
+	response := recorder.Result()
+	defer response.Body.Close()
+
+	responseBody, err := io.ReadAll(response.Body)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+
+	expectedResponse := `{
+		"id": 100,
+		"title": "Title",
+		"content": "Content"
+	}`
+
+	assert.JSONEq(t, expectedResponse, string(responseBody))
+}
+
+func TestPostHandler_SavePost(t *testing.T) {
+	engine, postService := newPostHandler(t)
+
+	post := model.Post{
+		Model: gorm.Model{
+			ID: 100,
+		},
+		Title:   "Title",
+		Content: "Content",
+	}
+
+	createPostRequest := request.CreatePostRequest{
+		BasicPost: &request.BasicPost{
+			Title:   "Title",
+			Content: "Content",
+		},
+	}
+
+	rawCreatePostRequest, err := json.Marshal(createPostRequest)
+	require.NoError(t, err)
+
+	postService.
+		EXPECT().
+		CreatePost("Title", "Content", uint(101)).
+		Return(&post, nil)
+
+	httpRequest := httptest.NewRequest(http.MethodPost, "/posts", bytes.NewReader(rawCreatePostRequest))
 
 	recorder := httptest.NewRecorder()
 	engine.ServeHTTP(recorder, httpRequest)
