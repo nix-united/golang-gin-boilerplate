82eea72b9507f0d6534d52d448aac11b4ccf6082
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-01-12 08:36:52 +0200

    refactor: change post service style

--- internal/service/post_service.go
+++ internal/service/post_service.go
@@ -22,64 +22,14 @@ func NewPostService(postRepository postRepository) PostService {
 	return PostService{postRepository: postRepository}
 }
 
-func (service PostService) GetAll(posts *[]model.Post) *RestError {
-	if err := service.postRepository.GetAll(posts); err != nil {
-		return &RestError{
-			Status: http.StatusInternalServerError,
-			Error:  err,
-		}
-	}
-	return nil
-}
-
-func (service PostService) GetByID(id int, post *model.Post) *RestError {
-	if err := service.postRepository.GetByID(id, post); err != nil {
-		return &RestError{
-			Status: http.StatusInternalServerError,
-			Error:  err,
-		}
-	}
-	return nil
-}
-
-func (service PostService) Create(post *model.Post) *RestError {
-	if err := service.postRepository.Create(post); err != nil {
-		return &RestError{
-			Status: http.StatusInternalServerError,
-			Error:  err,
-		}
-	}
-	return nil
-}
-
-func (service PostService) Save(post *model.Post) *RestError {
-	if err := service.postRepository.Save(post); err != nil {
-		return &RestError{
-			Status: http.StatusInternalServerError,
-			Error:  err,
-		}
-	}
-	return nil
-}
-
-func (service PostService) Delete(post *model.Post) *RestError {
-	if err := service.postRepository.Delete(post); err != nil {
-		return &RestError{
-			Status: http.StatusInternalServerError,
-			Error:  err,
-		}
-	}
-	return nil
-}
-
-func (service PostService) CreatePost(title, content string, userID uint) (*model.Post, *RestError) {
+func (s PostService) CreatePost(title, content string, userID uint) (*model.Post, *RestError) {
 	post := &model.Post{
 		Title:   title,
 		Content: content,
 		UserID:  userID,
 	}
 
-	if err := service.postRepository.Create(post); err != nil {
+	if err := s.postRepository.Create(post); err != nil {
 		return nil, &RestError{
 			Status: http.StatusInternalServerError,
 			Error:  err,
@@ -87,4 +37,59 @@ func (service PostService) CreatePost(title, content string, userID uint) (*mode
 	}
 
 	return post, nil
+}
+
+func (s PostService) Create(post *model.Post) *RestError {
+	if err := s.postRepository.Create(post); err != nil {
+		return &RestError{
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+
+	return nil
+}
+
+func (s PostService) GetAll(posts *[]model.Post) *RestError {
+	if err := s.postRepository.GetAll(posts); err != nil {
+		return &RestError{
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+
+	return nil
+}
+
+func (s PostService) GetByID(id int, post *model.Post) *RestError {
+	if err := s.postRepository.GetByID(id, post); err != nil {
+		return &RestError{
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+
+	return nil
+}
+
+func (s PostService) Save(post *model.Post) *RestError {
+	if err := s.postRepository.Save(post); err != nil {
+		return &RestError{
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+
+	return nil
+}
+
+func (s PostService) Delete(post *model.Post) *RestError {
+	if err := s.postRepository.Delete(post); err != nil {
+		return &RestError{
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+
+	return nil
 }
