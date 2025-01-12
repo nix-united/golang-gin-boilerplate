154064bb92dd77ba7947fc0e11263ccfb81529c2
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-01-12 08:31:40 +0200

    refactor: add local interface to post service

--- internal/service/post_service.go
+++ internal/service/post_service.go
@@ -4,24 +4,26 @@ import (
 	"net/http"
 
 	"github.com/nix-united/golang-gin-boilerplate/internal/model"
-	"github.com/nix-united/golang-gin-boilerplate/internal/repository"
 )
 
-type PostServiceI interface {
-	CreatePost(title, content string, userID uint) (*model.Post, *RestError)
-	GetAll(posts *[]model.Post) *RestError
-	GetByID(id int, post *model.Post) *RestError
-	Create(post *model.Post) *RestError
-	Save(post *model.Post) *RestError
-	Delete(post *model.Post) *RestError
+type postRepository interface {
+	GetAll(posts *[]model.Post) error
+	GetByID(id int, post *model.Post) error
+	Create(post *model.Post) error
+	Save(post *model.Post) error
+	Delete(post *model.Post) error
 }
 
 type PostService struct {
-	PostRepository repository.PostRepositoryI
+	postRepository postRepository
 }
 
-func (service *PostService) GetAll(posts *[]model.Post) *RestError {
-	if err := service.PostRepository.GetAll(posts); err != nil {
+func NewPostService(postRepository postRepository) PostService {
+	return PostService{postRepository: postRepository}
+}
+
+func (service PostService) GetAll(posts *[]model.Post) *RestError {
+	if err := service.postRepository.GetAll(posts); err != nil {
 		return &RestError{
 			Status: http.StatusInternalServerError,
 			Error:  err,
@@ -30,8 +32,8 @@ func (service *PostService) GetAll(posts *[]model.Post) *RestError {
 	return nil
 }
 
-func (service *PostService) GetByID(id int, post *model.Post) *RestError {
-	if err := service.PostRepository.GetByID(id, post); err != nil {
+func (service PostService) GetByID(id int, post *model.Post) *RestError {
+	if err := service.postRepository.GetByID(id, post); err != nil {
 		return &RestError{
 			Status: http.StatusInternalServerError,
 			Error:  err,
@@ -40,8 +42,8 @@ func (service *PostService) GetByID(id int, post *model.Post) *RestError {
 	return nil
 }
 
-func (service *PostService) Create(post *model.Post) *RestError {
-	if err := service.PostRepository.Create(post); err != nil {
+func (service PostService) Create(post *model.Post) *RestError {
+	if err := service.postRepository.Create(post); err != nil {
 		return &RestError{
 			Status: http.StatusInternalServerError,
 			Error:  err,
@@ -50,8 +52,8 @@ func (service *PostService) Create(post *model.Post) *RestError {
 	return nil
 }
 
-func (service *PostService) Save(post *model.Post) *RestError {
-	if err := service.PostRepository.Save(post); err != nil {
+func (service PostService) Save(post *model.Post) *RestError {
+	if err := service.postRepository.Save(post); err != nil {
 		return &RestError{
 			Status: http.StatusInternalServerError,
 			Error:  err,
@@ -60,8 +62,8 @@ func (service *PostService) Save(post *model.Post) *RestError {
 	return nil
 }
 
-func (service *PostService) Delete(post *model.Post) *RestError {
-	if err := service.PostRepository.Delete(post); err != nil {
+func (service PostService) Delete(post *model.Post) *RestError {
+	if err := service.postRepository.Delete(post); err != nil {
 		return &RestError{
 			Status: http.StatusInternalServerError,
 			Error:  err,
@@ -70,14 +72,14 @@ func (service *PostService) Delete(post *model.Post) *RestError {
 	return nil
 }
 
-func (service *PostService) CreatePost(title, content string, userID uint) (*model.Post, *RestError) {
+func (service PostService) CreatePost(title, content string, userID uint) (*model.Post, *RestError) {
 	post := &model.Post{
 		Title:   title,
 		Content: content,
 		UserID:  userID,
 	}
 
-	if err := service.PostRepository.Create(post); err != nil {
+	if err := service.postRepository.Create(post); err != nil {
 		return nil, &RestError{
 			Status: http.StatusInternalServerError,
 			Error:  err,
@@ -85,8 +87,4 @@ func (service *PostService) CreatePost(title, content string, userID uint) (*mod
 	}
 
 	return post, nil
-}
-
-func NewPostService(postRepo repository.PostRepositoryI) PostServiceI {
-	return &PostService{PostRepository: postRepo}
 }
