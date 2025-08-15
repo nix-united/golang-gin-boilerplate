dea75f5d6e20fb5129983ecc98dee6ef27804f48
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-08-15 11:26:38 +0300

    linter: fix issues with db connection

--- internal/db/db.go
+++ internal/db/db.go
@@ -10,18 +10,18 @@ import (
 	"gorm.io/gorm"
 )
 
-func NewDBConnection(cfg config.DBConfig) (*gorm.DB, *sql.DB, error) {
+func NewDBConnection(cfg config.DBConfig) (gormDB *gorm.DB, sqlDB *sql.DB, err error) {
 	dataSourceName := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name,
 	)
 
-	sqlDB, err := sql.Open(cfg.Driver, dataSourceName)
+	sqlDB, err = sql.Open(cfg.Driver, dataSourceName)
 	if err != nil {
 		return nil, nil, fmt.Errorf("open db connection: %w", err)
 	}
 
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{Conn: sqlDB}), &gorm.Config{Logger: newLoggerAdapter()})
+	gormDB, err = gorm.Open(mysql.New(mysql.Config{Conn: sqlDB}), &gorm.Config{Logger: newLoggerAdapter()})
 	if err != nil {
 		return nil, nil, fmt.Errorf("open gorm session: %w", err)
 	}
