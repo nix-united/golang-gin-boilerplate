705214405cde9789e63f016ce47c0a66b6c042bc
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-08-29 09:05:45 +0300

    feat: add logging request debugger

---
+++ internal/middleware/request_debugger.go
@@ -0,0 +1,119 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// requestDebugger is a logging middleware that logs request and response bodies with DEBUG level logs.
+// Warning: Do not use this middleware with endpoints containing sensitive information.
+//
+// Based on the Content-Type, it determines how the body will be formatted.
+// If the content type is application/json, the body will be logged as JSON; otherwise, it will be logged as a string.
+type requestDebugger struct{}
+
+func NewRequestDebugger() gin.HandlerFunc {
+	return (&requestDebugger{}).handle
+}
+
+func (d *requestDebugger) handle(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	if !slog.Default().Enabled(ctx, slog.LevelDebug) {
+		return
+	}
+
+	requestBody, err := d.getRequestBody(c)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to read request body for request debugging", "err", err.Error())
+
+		return
+	}
+
+	responseBodyGetter := d.getResponseBodyGetter(c)
+
+	c.Next()
+
+	var attrs []any
+	if requestBody != nil {
+		attrs = append(attrs, "request_body", requestBody)
+	}
+
+	responseBody := responseBodyGetter(c)
+	if responseBody != nil {
+		attrs = append(attrs, "response_body", responseBody)
+	}
+
+	message := "Request/response data"
+	if len(attrs) == 0 {
+		message = "Request/response without any data"
+	}
+
+	slog.DebugContext(c.Request.Context(), message, attrs...)
+
+	return
+}
+
+func (d *requestDebugger) getRequestBody(c *gin.Context) (any, error) {
+	if c.Request.Body == nil {
+		return nil, nil
+	}
+
+	rawRequestBody, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read request body: %w", err)
+	}
+
+	c.Request.Body = io.NopCloser(bytes.NewReader(rawRequestBody))
+
+	if strings.HasPrefix(c.Request.Header.Get("Content-Type"), "application/json") {
+		return json.RawMessage(rawRequestBody), nil
+	}
+
+	return string(rawRequestBody), nil
+}
+
+func (d *requestDebugger) getResponseBodyGetter(c *gin.Context) func(c *gin.Context) any {
+	storer := newResponseStorer(c.Writer)
+	c.Writer = storer
+
+	return func(c *gin.Context) any {
+		if storer.storedResponse == nil {
+			return nil
+		}
+
+		if strings.HasPrefix(c.Request.Response.Header.Get("Content-Type"), "application/json") {
+			return json.RawMessage(storer.storedResponse)
+		}
+
+		return string(storer.storedResponse)
+	}
+}
+
+// responseStorer stores the written response by the handler into its field.
+// This is used to automate response logging.
+type responseStorer struct {
+	gin.ResponseWriter
+	storedResponse []byte
+}
+
+func newResponseStorer(writer gin.ResponseWriter) *responseStorer {
+	return &responseStorer{ResponseWriter: writer}
+}
+
+func (s *responseStorer) Write(response []byte) (int, error) {
+	s.storedResponse = response
+
+	n, err := s.ResponseWriter.Write(response)
+	if err != nil {
+		return n, fmt.Errorf("write response with storer: %w", err)
+	}
+
+	return n, nil
+}
