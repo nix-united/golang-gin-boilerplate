6d5669dd84994b74e2f946e156df6786c73c8f5e
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-08-29 08:42:50 +0300

    feat: add http server request logger

---
+++ internal/middleware/request_logger.go
@@ -0,0 +1,53 @@
+package middleware
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type tracer interface {
+	Start(ctx context.Context) (context.Context, error)
+}
+
+// requestLogger is a logging middleware that generated trace ID for each request.
+type requestLogger struct {
+	tracer tracer
+}
+
+func NewRequestLogger(tracer tracer) gin.HandlerFunc {
+	return (&requestLogger{tracer: tracer}).handle
+}
+
+// handle creates trace and logs request information.
+func (l *requestLogger) handle(c *gin.Context) {
+	ctx, err := l.tracer.Start(c.Request.Context())
+	if err != nil {
+		slog.ErrorContext(c.Request.Context(), "Failed to sart a trace", "err", err.Error())
+
+		return
+	}
+
+	c.Request = c.Request.WithContext(ctx)
+
+	c.Next()
+
+	level := slog.LevelInfo
+	if c.Request.Response.StatusCode >= http.StatusInternalServerError {
+		level = slog.LevelError
+	}
+
+	attrs := []any{
+		"method", c.Request.Method,
+		"status", c.Request.Response.Status,
+		"path", c.Request.URL.Path,
+	}
+
+	if len(c.Errors) > 0 {
+		attrs = append(attrs, "error", c.Errors.JSON())
+	}
+
+	slog.Log(c.Request.Context(), level, "Request", slog.Group("http", attrs...))
+}
